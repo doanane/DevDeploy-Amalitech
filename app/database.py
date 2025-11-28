93e77e9d1904f67165355c250e1208ceb32ea523
doanane <anane365221@gmail.com>
2025-11-28 21:21:46 +0000

    updated config.py

--- app/database.py
+++ app/database.py
@@ -10,11 +10,11 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine=create_engine(DATABASE_URL, echo=True)
 
-SessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=True)
+SessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base= declarative_base()
 
 def get_db():
-    db= SsseionLocal
+    db= SessionLocal()
     try:
         yield db
     finally:
