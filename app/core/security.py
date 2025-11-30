146a6439c1b1880ec3d2d40c55bbf777f2a7c06f
doanane <anane365221@gmail.com>
2025-11-30 16:17:44 +0000

    implemented security verification ^

--- app/core/security.py
+++ app/core/security.py
@@ -1,34 +1,35 @@
-from dotenv import load_dotenv
-from jose import jwt, JWTError
-from passlib.context import CryptContext
 from app.core.config import settings
-from datetime import datetime, timedelta, timezone
+from jose import JWTError, jwt
+from dotenv import load_dotenv
+from passlib.context import CryptContext
+from datetime import timezone, datetime, timedelta
 
-# my environment that specifies password hashing algorithm and JWT settings types
-pwd_context =CryptContext(schemes=["bcrypt"], deprecated= "auto")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated= "auto")
 
 load_dotenv()
 
-def verify_password(plain_password: str, hashed_password: str) -> bool:
+def verify_password(plain_password:str, hashed_password:str)-> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
-def get_password_hash(password: str) -> str:
-    if len(password.encode("utf-8")) > 72:
-        raise ValueError("Password length exceeds maximum limit of 72 characters.")
+def get_password_hash(password: str)-> str:
+    if len(password.encode('utf-8')) >72:
+        raise ValueError("password is too long")
     return pwd_context.hash(password)
 
-def create_access_token(data:dict) -> str:
-    expire= datetime.now(timezone.utc) + timedelta(minutes= settings.access_token_expire_minutes)
+def create_access_token(data: dict)-> str:
+    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.access_token_expire_minutes)
 
-    data.update({"exp": expire})
-    encoded_jwt =jwt.encode(data, settings.secret_key, algorithm= settings.algorithm)
+    date.update=({"exp": expire})
+
+    encoded_jwt=jwt.encode(data, settings.secret_key, algotithm=settings.algorithm)
+
     return encoded_jwt
 
-def verify_token(token:str)-> str:
+def verify_token(token: str)-> str:
     try:
-        payload= jwt.decode(token, settings.secret_key, algorithms= [settings.algorithm])
+        payload= jwt.decode(token, settings.secret_key, algorithms=[settings.algorithm])
         email= payload.get("sub")
-        return email    
-    except JWTError:
-        return None
+        return email
 
+    except JWTError:
+        None
